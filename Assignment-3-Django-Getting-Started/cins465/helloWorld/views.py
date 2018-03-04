9b5aaed77b7231e718d27626b3438b5042658738
aswingler1 <36062866+aswingler1@users.noreply.github.com>
2018-03-04 17:32:28 -0500

    Add files via upload

---
+++ Assignment-3-Django-Getting-Started/cins465/helloWorld/views.py
@@ -0,0 +1,12 @@
+
+
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    #return HttpResponse("Hello World")
+    return render(request, 'index.html')
+
